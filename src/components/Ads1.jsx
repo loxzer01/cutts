6e5fa4713cf729a971307b0b12231e6b205516da
Loxzer01 <javierover04@gmail.com>
2022-05-22 19:33:04 -0400

    actualizar

--- src/components/Ads1.jsx
+++ src/components/Ads1.jsx
@@ -1,34 +1,30 @@
-export default function Ads2(){
-
-    return (
-        <>
-        
-        <div className="ads">
-            <iframe
-            data-aa="2011983"
-            src="//ad.a-ads.com/2011983?size=728x90"
-            className="iframe"
-            ></iframe> 
-        </div>
-        <style jsx>{`
-           
-            .ads {
-                display: grid;
-                place-content: center;
-                padding: 1rem 0;
-                background: var(--color-1);
-                overflow: hidden;
-            }
-            .iframe{
-                width:728px;
-                height:90px;
-                border:0px;
-                padding:0;
-                overflow:hidden;
-                background-color: transparent;
-                }
-
-        `}</style>
-        </>
-    )
+export default function Ads2() {
+  return (
+    <>
+      <div className="ads">
+        <iframe
+          data-aa="2014093"
+          src="//ad.a-ads.com/2014093?size=728x90"
+          className="iframe"
+        ></iframe>
+      </div>
+      <style jsx>{`
+        .ads {
+          display: grid;
+          place-content: center;
+          padding: 1rem 0;
+          background: var(--color-1);
+          overflow: hidden;
+        }
+        .iframe {
+          width: 728px;
+          height: 90px;
+          border: 0px;
+          padding: 0;
+          overflow: hidden;
+          background-color: transparent;
+        }
+      `}</style>
+    </>
+  );
 }
