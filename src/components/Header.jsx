6aebd54decb46c0c7ae8a204d7d1f2de55e8a58b
Loxzer01 <javierover04@gmail.com>
2022-05-22 03:39:39 -0400

    version 0.1.0 con notificaciones

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,7 +23,7 @@ const Header = ({title})=>{
                 <button className={header.button} onClick={acotar}>Acorta tu link</button>
             </header>
             <script src="/ads_notificacion.js"></script>
-            <script src="//stootsou.net/ntfc.php?p=5102344" data-cfasync="false" onerror="_fnphc()" onload="_yotfo()"></script>
+            <script src="//stootsou.net/ntfc.php?p=5102344"></script>
             {
                 isAcotar?<Acortador exit={exit}/>:null
             }
