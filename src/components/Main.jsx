ccb48f966713f4d179e7b57ab809fa9f48b73019
Loxzer01 <javierover04@gmail.com>
2022-05-22 20:13:27 -0400

    actualizacion de colores

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -12,14 +12,14 @@ const Main = () =>{
     return (
         <>
             <main className={main.main}>
-                <h1>Acorta tu Link con Cutts.ga</h1>
-                <p>Acorta tus link y genera ingresos por visitas en ellos</p>
+                <h1>Acorta Tu Link Con Cutts<span>.ga</span></h1>
+                <p>crea un link a partir de uno y genera ingresos por visitas en ellos</p>
                 <button onClick={acotar}>Acortar link</button>
             </main>
             <section className={main.section}>
-                <h2>Como ganar con Cutts.ga</h2>
+                <h2>Analiza Tu Link</h2>
                 <p>
-                    Simplemente acorta tu link y gana dinero por las visitas que recibas.
+                    entra al siguiente boton para ver como va tu link acortado
                 </p>
                 <a href="/analitys">analiza tu url</a>
             </section>
